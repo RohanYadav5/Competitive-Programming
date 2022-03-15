6d029992d24167d6e1133e1e78a9c18974b18568
RohanYadav5 <98342900+RohanYadav5@users.noreply.github.com>
2022-03-15 10:15:16 +0530

    added template with single type pass

---
+++ template/singleTemplate.cpp
@@ -0,0 +1,31 @@
+// creating one template handler using template in c++
+template<typename T>
+
+// making class using template
+class Pair{
+  T x;
+  T y;
+  
+  public:
+  
+  void setX(T x){
+    this->x=x 
+  }
+  
+  T getX(){ return x; }
+  
+   void setY(T y){
+    this->y=y 
+  }
+  
+  T getY(){ return y; }
+
+}
+
+
+int main(){
+  
+  //using pair class for the templete
+  Pair<int,string> p1;
+  
+}
