124a0300b0eb8e40f4d79a52c5467aa5359f7d56
RohanYadav5 <98342900+RohanYadav5@users.noreply.github.com>
2022-03-16 00:34:19 +0530

    stacks using c++ inbuild library

---
+++ Stacks/inbuildStacks.cpp
@@ -0,0 +1,12 @@
+//using c++ inbuild library for stack implemetation 
+#includes<stack>
+
+//stack<type of stacks> stack name
+
+stack<int> st;
+
+st.push(10);
+st.top();
+st.pop();
+st.size();
+st.isEmpty();
