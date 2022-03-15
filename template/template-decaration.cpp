6895c9739ecf14b8df0f2fcda5f1eff5f0755091
RohanYadav5 <98342900+RohanYadav5@users.noreply.github.com>
2022-03-15 09:59:46 +0530

    declaration file for template

---
+++ template/template-decaration.cpp
@@ -0,0 +1,12 @@
+//templates are used when we don't won't our code just to be handling on type of data
+//it make the class more reusable 
+//handles different types of data and no need for changing the data type
+
+//declaration
+
+//declaring one type of data type
+template<typename T>
+
+//declaring 2 or more type of data types
+template<typename T , typename V>
+
