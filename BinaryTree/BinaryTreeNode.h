36ba92b02d125657e72c8b18bba1c4123ba7bccb
RohanYadav5 <98342900+RohanYadav5@users.noreply.github.com>
2022-04-25 10:16:56 +0530

    create binary tree node class

---
+++ BinaryTree/BinaryTreeNode.h
@@ -0,0 +1,27 @@
+
+template<typename T>
+class BinaryTreeNode{
+    Public:
+    T data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+
+    //constructor
+    BinaryTreeNode(){
+        this->data=data;
+        //to avoid garbage
+        left=NULL;
+        right=NULL;
+    }
+
+    //destructor
+    ~BinaryTreeNode(){
+        //this will automatically ignore if left or right is null
+        //also it is a recursive function the end nodes are deleted first and
+        //then the first root node is deleted at last
+        delete left;
+        delete right;
+    }
+
+
+}
